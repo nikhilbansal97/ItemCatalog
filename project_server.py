b38301539b90e97eebaa828751789d2933751e9e
Nikhil Bansal <nikhil97.nb@gmail.com>
2018-03-08 00:50:00 +0530

    Implemented CRUD Add operation for movies and heroes

--- project_server.py
+++ project_server.py
@@ -11,25 +11,41 @@ Base.metadata.bind = engine
 Session = sessionmaker(bind = engine)
 session = Session()
 
-
 @app.route('/')
 @app.route('/marvels/movies', methods = ['GET', 'POST'])
 def showMovies():
 	moviesList = session.query(Movie).all()
-	return render_template('index.html', moviesList = moviesList)
+	return render_template('index.html', moviesList = moviesList, showContent = "movies")
+
+@app.route('/marvels/heroes', methods = ['GET', 'POST'])
+def showHeroes():
+	heroesList = session.query(Character).all()
+	return render_template('index.html', heroesList = heroesList, showContent = "heroes")
 
 @app.route('/marvels/movies/new', methods = ['GET','POST'])
 def newMovie():
-	if request.method == "post":
-		
-	else
+	if request.method == 'POST':
+		name = request.form['inputName']
+		description = request.form['inputDescription']
+		link = request.form['inputLink']
+		movie = Movie(name = name, description = description, image_path = link)
+		session.add(movie)
+		session.commit()
+		return redirect(url_for('showMovies'))
+	else:
 		return render_template('newForm.html', new = 'movie')
 
 
 @app.route('/marvels/heroes/new', methods = ['GET', 'POST'])
 def newHero():
-	if request.method == "post":
-
+	if request.method == 'POST':
+		name = request.form['inputName']
+		description = request.form['inputDescription']
+		link = request.form['inputLink']
+		hero = Character(name = name, description = description, image_path = link)
+		session.add(hero)
+		session.commit()
+		return redirect(url_for('showHeroes'))
 	else:
 		return render_template('newForm.html', new = 'hero')
 
